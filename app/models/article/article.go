578ea9f957bdb3e143e08df078b298de3d186a44
TingyuSleep <525605299@qq.com>
2024-05-17 20:55:39 +0800

    新建 Article 模型

---
+++ app/models/article/article.go
@@ -0,0 +1,10 @@
+// Package article 应用的文章模型
+package article
+
+// Article 文章模型
+type Article struct {
+	//将 ID 从 int64 修改为 uint64 ，因为数据库的 ID 为大于 0 的自增整数
+	ID    uint64
+	Title string
+	Body  string
+}
