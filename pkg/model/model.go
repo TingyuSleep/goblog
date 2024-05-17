db11292aa9eda61fcedacd45a5bb91e2c7a9bb01
TingyuSleep <525605299@qq.com>
2024-05-17 21:15:51 +0800

    重构文章列表页面

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -5,6 +5,8 @@ import (
 	"goblog/pkg/logger"
 
 	"gorm.io/gorm"
+	//名称冲突，故为其指定名称
+	gormlogger "gorm.io/gorm/logger"
 
 	// GORM 的 MySQL 数据库驱动导入
 	"gorm.io/driver/mysql"
@@ -23,7 +25,9 @@ func ConnectDB() *gorm.DB {
 	})
 
 	// 准备数据库连接池
-	DB, err = gorm.Open(config, &gorm.Config{})
+	DB, err = gorm.Open(config, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	})
 
 	logger.LogError(err)
 
